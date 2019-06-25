0beb54caa380991bcbd1129d3b8daa0cfd4ef0df
Andy Boyle <andy@sudostream.io>
2019-06-25 06:37:35 +0100

    add reactive nature

--- src/main/kotlin/io/sudostream/myfoodweek/controller/DashboardController.kt
+++ src/main/kotlin/io/sudostream/myfoodweek/controller/DashboardController.kt
@@ -2,14 +2,15 @@ package io.sudostream.myfoodweek.controller
 
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import reactor.core.publisher.Mono
 import java.time.LocalDateTime
 
 @RestController
 class DashboardController {
 
     @RequestMapping("/hello")
-    fun helloWord(): String {
-        return "Hey there, the time is " + LocalDateTime.now() + "\n"
+    fun helloWord(): Mono<String> {
+        return Mono.just("Hey there! And the time is " + LocalDateTime.now() + "\n")
     }
 
 }
