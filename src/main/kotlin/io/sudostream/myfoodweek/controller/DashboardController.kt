dc9fdc34a80f0f855a3b1d596073a8f85cbd2f82
Andy Boyle <andy@sudostream.io>
2019-06-26 18:14:25 +0100

    use GetMapping

--- src/main/kotlin/io/sudostream/myfoodweek/controller/DashboardController.kt
+++ src/main/kotlin/io/sudostream/myfoodweek/controller/DashboardController.kt
@@ -1,6 +1,6 @@
 package io.sudostream.myfoodweek.controller
 
-import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 import reactor.core.publisher.Mono
 import java.time.LocalDateTime
@@ -8,7 +8,7 @@ import java.time.LocalDateTime
 @RestController
 class DashboardController {
 
-    @RequestMapping("/hello")
+    @GetMapping("/hello")
     fun helloWord(): Mono<String> {
         return Mono.just("Hey there! And the time is " + LocalDateTime.now() + "\n")
     }
